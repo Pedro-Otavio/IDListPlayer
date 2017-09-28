1a5236dced006d6bb98ee362d9331ddd9690ea28
pedro-otavio <pedro_otavio@outlook.com>
2017-09-28 18:53:04 -0300

    JSON!

--- IDListPlayer.js
+++ IDListPlayer.js
@@ -1,21 +1,15 @@
 let player;
-let titleArr;
+let fileReaderObject = new FileReader();
+let fileData;
+let titleArray;
 let titleIndex = 0;
-let minQual = false;
-let currVidMinQual = false;
-let IDList;
+let minQuality = false;
+let currVidMinQuality = false;
+let indexArray;
 let index = 0;
 let max;
 
-function resize() {
-    let plr = $('#player');
-    let w = Math.min(document.documentElement.clientWidth, 768);
-    let h = Math.floor(w * 9 / 16);
-    plr.attr('width', w);
-    plr.attr('height', h);
-}
-
-function onYouTubeIframeAPIReady() {
+function onYouTubeIframeAPIReady() { //eslint-disable-line no-unused-vars
     window.setTimeout(function () {
         player = new YT.Player('player', {
             events: {
@@ -28,21 +22,53 @@ function onYouTubeIframeAPIReady() {
     resize();
 
     window.addEventListener('orientationchange', function () {
-        window.setTimeout(resize, 640);
+        window.setTimeout(resize, 256);
     });
 
     window.setInterval(titleSwitch, 1500);
+
+    $('#fileInput').change(readFile);
+    $('#shuffle').click(shufflePlaylist);
+    $('#next').click(next);
+    $('#previous').click(previous);
+    $('#skipTo').click(function () {
+        skipTo($('#indexInput').val);
+    });
+    $('#toggleMinQuality').click(toggleMinQuality);
+    $('#save').click(save);
+}
+
+fileReaderObject.onerror = function (event) {
+    $('#errow').show();
+    $('#errorMsg').text("\nFileReader error code " + event.target.error.code + "\n");
+};
+
+fileReaderObject.onload = function (event) {
+    fileData = JSON.parse(event.target.result);
+    ready();
+};
+
+function readFile() {
+    fileReaderObject.readAsText($('#fileInput').prop('files')[0]);
+}
+
+function ready() {
+    indexArray = [...Array(fileData.length).keys()];
+    enableBtns();
+    setIndex(0);
+    player.cueVideoById(fileData[0].id);
+    $('#fileInput').val(null);
 }
 
 function playerChanged(event) {
     switch (event.data) {
         case 0:
-            skipTo(index + 1);
+            next();
             break;
         case 1:
-            if (titleArr == null) {
-                titleArr = splitArtistSong(player.getVideoData().title);
-                document.title = titleArr[0];
+            if (titleArray == null) {
+                titleArray = splitArtistSong(player.getVideoData().title);
+                document.title = titleArray[0];
             }
             break;
         case 3:
@@ -51,93 +77,28 @@ function playerChanged(event) {
     }
 }
 
-function quality() {
-    if (minQual && !currVidMinQual) {
-        player.setPlaybackQuality('small');
-        player.setPlaybackQuality('tiny');
-        currVidMinQual = true;
-    }
-}
-
-function splitArtistSong(title) {
-    let dash = title.indexOf(" - ");
-    if (dash != -1) {
-        return [title.substring(0, dash), title.substring(dash + 3)];
-    } else {
-        return [title];
-    }
-}
-
-function titleSwitch() {
-    if (titleArr) {
-        document.title = titleArr[titleIndex];
-        titleIndex++;
-        if (titleIndex > (titleArr.length - 1)) {
-            titleIndex = 0;
-        }
-    }
-}
-
-function toggleMinQuality() {
-    minQual = !minQual;
-    $('#labelMinquality').toggleClass('disabled');
-    quality();
-}
-
-function enableBtns() {
-    $('#previous').prop('disabled', false);
-    $('#next').prop('disabled', false);
-    $('#skipTo').prop('disabled', false);
-    $('#shuffle').prop('disabled', false);
-    $('#save').prop('disabled', false);
-    $('#labelPrevious').removeClass('disabled');
-    $('#labelNext').removeClass('disabled');
-    $('#labelSkipTo').removeClass('disabled');
-    $('#labelShuffle').removeClass('disabled');
-    $('#labelSave').removeClass('disabled');
-}
-
-let reader = new FileReader();
-reader.onloadend = function (event) {
-    if (event.target.error) {
-        $('#errow').show();
-        console.log("\nFileReader error code " + event.target.error.code + "\n");
-    } else {
-        readIDs(event.target.result);
-        enableBtns();
-        setIndex(0);
-        player.cueVideoById(IDList[0]);
-    }
-};
-
-function ready() {
-    let IDstxt = $('#fileInput').prop('files')[0];
-    reader.readAsText(IDstxt);
-}
-
-function readIDs(fileIDs) {
-    IDList = fileIDs.split('~');
-    max = IDList.length - 1;
-    $('#fileInput').val(null);
-}
-
-let rnd = Math.random;
-
-function shuffle(arr, len = arr.length, i, k) {
-    while (len) i = rnd() * len-- | 0, k = arr[len], arr[len] = arr[i], arr[i] = k;
-}
-
-function shuffleList() {
-    shuffle(IDList);
+function shufflePlaylist() {
+    shuffleArray(indexArray);
     skipTo(0);
+}
+
+function play() {
+    currVidMinQuality = false;
+    titleArray = null;
+    player.loadVideoById(fileData[indexArray[index]].id);
 }
 
 function skipTo(i) {
     setIndex(i);
-    document.title = "ID List Player";
-    currVidMinQual = false;
-    titleArr = null;
-    player.loadVideoById(IDList[index]);
+    play();
+}
+
+function next() {
+    skipTo(index + 1);
+}
+
+function previous() {
+    skipTo(index - 1);
 }
 
 function setIndex(i) {
@@ -151,21 +112,78 @@ function setIndex(i) {
 }
 
 function save() {
-    let IDs = IDList.join('~');
+    let IDs = indexArray.join('~');
     let blob = new Blob([IDs], {
         type: 'text/plain'
     });
     let url = window.URL.createObjectURL(blob);
-    let a = $('#download');
-    a.attr('href', url);
-    a.attr('download', 'ShuffledIDs.txt');
-    a.attr('onclick', "downloaded('" + url + "')");
-    a.removeClass('hidden');
+    $('#download').attr('href', url).attr('download', 'ShuffledIDs.txt').click(function () {
+        downloaded(url);
+    }).removeClass('hidden');
 }
 
 function downloaded(url) {
     window.setTimeout(function () {
         window.URL.revokeObjectURL(url);
     }, 500);
-    $('#download').addClass('hidden');
+    $('#download').addClass('hidden').off();
+}
+
+function toggleMinQuality() {
+    minQuality = !minQuality;
+    $('#labelMinquality').toggleClass('disabled');
+    quality();
+}
+
+function quality() {
+    if (minQuality && !currVidMinQuality) {
+        player.setPlaybackQuality('small');
+        player.setPlaybackQuality('tiny');
+        currVidMinQuality = true;
+    }
+}
+
+function titleSwitch() {
+    if (titleArray) {
+        document.title = titleArray[titleIndex];
+        titleIndex = (titleIndex == (titleArray.length - 1)) ? titleIndex + 1 : 0;
+    } else {
+        document.title = "ID List Player";
+    }
+}
+
+function resize() {
+    let plr = $('#player');
+    let w = Math.max(Math.min(document.documentElement.clientWidth, 768), 232);
+    let h = Math.floor(w * 9 / 16);
+    plr.attr('width', w);
+    plr.attr('height', h);
+}
+
+function splitArtistSong(title) {
+    let separator = title.indexOf(" - ");
+    if (separator != -1) {
+        return [title.substring(0, separator), title.substring(separator + 3)];
+    } else {
+        return [title];
+    }
+}
+
+let rnd = Math.random;
+
+function shuffleArray(arr, len = arr.length, i, k) {
+    while (len) i = rnd() * len-- | 0, k = arr[len], arr[len] = arr[i], arr[i] = k;
+}
+
+function enableBtns() {
+    $('#previous').prop('disabled', false);
+    $('#next').prop('disabled', false);
+    $('#skipTo').prop('disabled', false);
+    $('#shuffle').prop('disabled', false);
+    $('#save').prop('disabled', false);
+    $('#labelPrevious').removeClass('disabled');
+    $('#labelNext').removeClass('disabled');
+    $('#labelSkipTo').removeClass('disabled');
+    $('#labelShuffle').removeClass('disabled');
+    $('#labelSave').removeClass('disabled');
 }
